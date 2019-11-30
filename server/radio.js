670f65b8670849c18442216285555df2fa207081
rzubala@gmail.com <rzubala@gmail.com>
2019-11-30 17:50:48 +0100

    play and stop

--- server/radio.js
+++ server/radio.js
@@ -1,19 +1,25 @@
 import { MPC } from 'mpc-js'
+import express from 'express';
 
-var express = require("express");
-var app = express();
+const mpc = new MPC()
+mpc.connectTCP('192.168.0.3', 6600)
+
+const app = express();
 app.listen(3000, () => {
- console.log("Media Player Daemon REST Server running on port 3000");
+    console.log("Media Player Daemon REST Server running on port 3000");
 });
 
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
-var mpc = new MPC()
+app.get("/play", (req, res, next) => {
+    mpc.playback.play()
+    res.json("Play")
+})
 
-mpc.connectTCP('192.168.0.3', 6600)
-
-//mpc.playback.play()
-mpc.playback.stop()
+app.get("/stop", (req, res, next) => {
+    mpc.playback.stop()
+    res.json("Stop")
+})
 
