7ea37c4f4ac34421d97b296fa72eba762c726247
rzubala@gmail.com <rzubala@gmail.com>
2020-03-25 19:11:51 +0100

    change order

--- client-expo/data/initial_data.ts
+++ client-expo/data/initial_data.ts
@@ -1,8 +1,8 @@
 import Track from '../models/track'
 
 export const Tracks = [
-    new Track('1000', 'Muzo FM', 'http://stream4.nadaje.com:8002/muzo', 'https://pbs.twimg.com/profile_images/928678233817657345/cbbO1Mjr_400x400.jpg'),
-    new Track('1001', 'Melo Radio', 'http://redir.atmcdn.pl/sc/o2/Eurozet/live/meloradio.livx', 'https://gfx.meloradio.pl/design/meloradio/images/layout/logo-melo.png'),
+    new Track('1000', 'Melo Radio', 'http://redir.atmcdn.pl/sc/o2/Eurozet/live/meloradio.livx', 'https://gfx.meloradio.pl/design/meloradio/images/layout/logo-melo.png'),
+    new Track('1001', 'Muzo FM', 'http://stream4.nadaje.com:8002/muzo', 'https://pbs.twimg.com/profile_images/928678233817657345/cbbO1Mjr_400x400.jpg'),
     new Track('1002', 'ES: Cosquillas', 'http://www.kidspublicradio.got.net:8000/cosquillas', 'https://www.kidspublicradio.org/wp-content/uploads/2015/03/cosquillas_featured-300x292.png'),
     new Track('1003', 'EN: Lullaby', 'http://kidspublicradio2.got.net:8000/lullaby', 'https://www.kidspublicradio.org/wp-content/uploads/2015/03/lullaby_featured-300x292.png'),
     new Track('1004', 'EN: Pipsqueaks', 'http://kidspublicradio2.got.net:8000/pipsqueaks', 'https://www.kidspublicradio.org/wp-content/uploads/2015/03/pipsqueaks_featured-300x292.png'),
